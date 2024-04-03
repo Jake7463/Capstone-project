185e88789440747f151429b35ca62d1b291a3c5b
Jake7463 <jacobhick@gmail.com>
2024-04-04 02:13:42 +0300

    fixed some visual issues

--- src/Components/Specials.js
+++ src/Components/Specials.js
@@ -10,27 +10,27 @@ function Specials(){
             <section id="specials">
                 <article>
                     <img src={greek} alt="Greek Salad dish"/>
-                    <span>
+                    <span class="specialsSpan">
                         <h2>Greek Salad</h2>
-                        <p id="price">$12.99</p>
+                        <p class="price">$12.99</p>
                     </span>
-                    <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
+                    <p class="text1">The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
                 </article>
                 <article>
                     <img src={bruchetta} alt="Bruchetta dish"/>
-                    <span>
+                    <span class="specialsSpan">
                         <h2>Bruchetta</h2>
-                        <p id="price">$ 5.99</p>
+                        <p class="price">$ 5.99</p>
                     </span>
-                    <p>Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. </p>
+                    <p class="text1">Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. </p>
                 </article>
                 <article>
                     <img src={lemon} alt="Lemon Dessert dish"/>
-                    <span>
+                    <span class="specialsSpan">
                         <h2>Lemon Dessert</h2>
-                        <p id="price">$ 5.00</p>
+                        <p class="price">$ 5.00</p>
                     </span>
-                    <p>This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.</p>
+                    <p class="text1">This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.</p>
                 </article>
             </section>
         </section>
