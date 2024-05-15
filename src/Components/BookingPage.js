7658d9fa7bdb04f3b9ca44201ef5cf36e492ac5f
Jake7463 <jacobhick@gmail.com>
2024-05-15 06:08:46 +0300

    3/3 tests

--- src/Components/BookingPage.js
+++ src/Components/BookingPage.js
@@ -3,21 +3,23 @@ import Header from "./Header";
 import BookingForm from "./BookingForm";
 import { useReducer } from "react";
 
-function updateTimes (state, action) {
+export function updateTimes (state, action) {
     if (action.type === "times_by_date"){
         return state
     }
+    return state
+};
+
+export function initializeTimes(){
+    return ["17:30", "18:00", "18:30", "19:00", "20:00", "20:30"];
 };
 
 function BookingPage(){
-
-    function initializeTimes(){
-        return ["17:30", "18:00", "18:30", "19:00", "20:00", "20:30"];
-    };
-
+    
     function change (d){
         dispatch ({type: "times_by_date"})
     };
+
 
     const [availableTimes , dispatch] = useReducer(updateTimes, initializeTimes());
 
--- src/Components/BookingPage.test.js
+++ src/Components/BookingPage.test.js
@@ -1,9 +1,14 @@
-import { render } from "@testing-library/react";
-import {BookingPage, initializeTimes} from './BookingPage';
-
+// import { render } from "@testing-library/react";
+import {initializeTimes, updateTimes} from './BookingPage';
+import {availableTimes} from './BookingForm';
 
 test('InitializeTimes is set correctly to the proper array', () => {
-    render(<BookingPage />);
     const initTimes = ["17:30", "18:00", "18:30", "19:00", "20:00", "20:30"];
     expect(initializeTimes()).toEqual(initTimes);
 })
+
+test('updateTimes shows the current state', ()=>{
+    const initialState = ["17:30", "18:00", "18:30", "19:00", "20:00", "20:30"];
+    const action = { type: "times_by_date" };
+    expect(updateTimes(initialState, action)).toEqual(initialState);
+  });
