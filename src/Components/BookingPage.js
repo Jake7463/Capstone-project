6adcdad4c77ac3a27dc72d824dff3bfd7e4ffd17
Jake7463 <jacobhick@gmail.com>
2024-05-16 09:23:10 +0300

    mocked bad API, connected to 'API'

--- src/Components/BookingPage.js
+++ src/Components/BookingPage.js
@@ -2,7 +2,33 @@ import Footer from "./Footer";
 import Header from "./Header";
 import BookingForm from "./BookingForm";
 import { useReducer } from "react";
-import {fetchAPI, submitAPI} from './mockBrokenAPI'
+import MockBadAPI from "./MockBadAPI";
+
+
+const dates = [
+    ["17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00"],
+    ["17:30", "18:00", "18:30", "19:00", "20:00", "21:00"],
+    ["17:30",  "19:00", "19:30", "20:00", "21:00"],
+    ["17:30", "19:00",  "20:30", "21:00"],
+    ["18:00", "18:30", "19:00", "19:30", "20:00"],
+    ["17:30",  "18:30", "19:00", "19:30"],
+    ["18:00", "18:30", "19:00"],
+    ["17:30", "18:00","19:30", "20:00",  "21:00"],
+    ["17:30", "18:00", "18:30", "19:00", "19:30", "21:00"],
+    ["18:30", "20:30", "21:00"]
+];
+
+
+function fetchAPI (date) {
+        const randNum = Math.floor(Math.random()*10);
+        const mockTimes = dates[randNum];
+        console.log(mockTimes)
+        return mockTimes;
+};
+function submitAPI (data) {
+    return true;
+};
+
 
 
 export function initializeTimes(){
